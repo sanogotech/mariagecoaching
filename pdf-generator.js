047a83ae11565a231316e8de7a98fe27027574c7
SANOGOTECH <sanogotech@yahoo.fr>
2024-09-09 16:02:24 +0000

    Update pdf-generator.js

--- pdf-generator.js
+++ pdf-generator.js
@@ -1,1 +1,13 @@
+// pdf-generator.js
+import { jsPDF } from 'jspdf';
+
+document.getElementById('export-pdf').addEventListener('click', () => {
+    const doc = new jsPDF();
+
+    doc.text('Rapport d\'évaluation', 10, 10);
+    const results = document.getElementById('results').innerText;
+    doc.text(results, 10, 20);
+
+    doc.save('rapport-evaluation.pdf');
+});
 
