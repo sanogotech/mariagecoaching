b515dc9520387a6a67a53fc640d219f61f0d6cd3
SANOGOTECH <sanogotech@yahoo.fr>
2024-09-09 16:01:37 +0000

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,23 @@
+// dashboard.js
+import { db } from './firebase-config.js';
+import { collection, getDocs } from 'firebase/firestore';
+
+async function loadResults() {
+    const querySnapshot = await getDocs(collection(db, "responses"));
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '';
+    
+    querySnapshot.forEach(doc => {
+        const data = doc.data();
+        const resultDiv = document.createElement('div');
+        resultDiv.classList.add('result');
+        resultDiv.innerHTML = `
+            <h4>Réponse ID: ${doc.id}</h4>
+            <pre>${JSON.stringify(data, null, 2)}</pre>
+        `;
+        resultsDiv.appendChild(resultDiv);
+    });
+}
+
+loadResults();
 
